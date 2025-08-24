2daa82cc2e2168fb7314f5da881c933d80d44fd9
ivan-grozni-2 <3ivangrozni@gmail.com>
2025-08-24 22:52:52 +0300

    update

--- script.js
+++ script.js
@@ -1,46 +1,5 @@
 const list = document.getElementById("project-list");
 
-/*async function loadProjects(){
-    try{
-        const GitJSON = await fetch('https://api.github.com/users/ivan-grozni-2/repos')
-        const transJSON = await GitJSON.json();
-
-        const portfolios = transJSON
-            .filter(repo => !["skills-introduction-to-github"].includes(repo.name))
-            .sort((a, b) => new Date(b.update_at) - new Date(a.update_at));
-
-        //portfolios = transJSON;
-
-
-
-        portfolios.forEach(element => {
-            const card = document.createElement('div');
-            card.classList.add('project-card');
-            
-            const description = element.description?.trim() || 'No description provided yet.';
-            const homepage = element.homepage || `https://${element.owner.login}.github.io/${element.name}/`;
-
-            card.innerHTML = `
-                <h3>${formatTitle(element.name)}</h3>
-                <p>${description}</p>
-                <p><strong>Language:</strong> ${element.language || 'N/A'}</p>
-                <a href="${homepage}" target="_blank">Live Demo</a>
-                <a href="${element.html_url}" target="_blank">GitHub</a>
-            `;
-      
-            list.appendChild(card);
-            
-        });
-
-    }
-    catch(error) {
-        list.innerHTML = `<p> error ${error.message}</p>`
-
-    }
-}*/
-
-const GITUSER = "ivan-grozni-2";
-
 async function loadProjects() {
     try{
         const response = await fetch(`https://api.github.com/users/ivan-grozni-2/repos`);
