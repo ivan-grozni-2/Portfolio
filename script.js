196496465004695f28e3f1aad89f46b7075c3eea
ivan-grozni-2 <3ivangrozni@gmail.com>
2025-08-24 22:37:53 +0300

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const list = document.getElementById("project-list");
 
-async function loadProjects(){
+/*async function loadProjects(){
     try{
         const GitJSON = await fetch('https://api.github.com/users/ivan-grozni-2/repos')
         const transJSON = await GitJSON.json();
@@ -37,7 +37,51 @@ async function loadProjects(){
         list.innerHTML = `<p> error ${error.message}</p>`
 
     }
+}*/
+
+const GITUSER = "ivan-grozni-2";
+
+async function loadProjects() {
+    try{
+        const response = await fetch(`https:api.github.com/users/${GITUSER}/repos`);
+        const repos = await response.json();
+
+        const projectRepos = repos.filter(repo => !repo.fork && repo.name!="skills-introduction-to-github");
+
+        const grid = document.getElementById("projects-grid");
+
+        grid.innerHTML = "";
+
+        projectRepos.forEach(repo => {
+            const link = document.createElement("a");
+            link.setAttribute("href", `${repo.homepage || repo.html_url}` )
+            link.classList.add("noline");
+            
+            
+            const card = document.createElement("div");
+            card.classList.add("card");
+            card.classList.add("reveal");
+
+
+            link.innerHTML = `
+            <h3> ${repo.name} </h3>
+            <p>${repo.description?.trim()  || "no description avaliable"} </p>
+            <a href = "${repo.homepage || repo.html_url}" target = "_blank" class = "btn"> View Project </a>
+            <a href = "${repo.html_url}" target = "_blank" class = "btn-outline"> Source Code </a>
+            `;
+
+            
+            grid.appendChild(card);
+            card.append(link);
+
+        });
+    } catch(error){
+        console.error("Error loading projects:", error);
+    }
+    
 }
+
+document.addEventListener("DOMContentLoaded", loadProjects);
 
 function formatTitle(name){
     return name
@@ -48,3 +92,42 @@ function formatTitle(name){
 }
 
 loadProjects();
+
+function revealOnScroll() {
+    const reveals = document.querySelectorAll(".reveal");
+    const windowHeight = window.innerHeight;
+  
+    reveals.forEach(el => {
+      const elementTop = el.getBoundingClientRect().top;
+      if (elementTop < windowHeight - 100) {
+        el.classList.add("active");
+      }
+    });
+
+  }
+
+  window.addEventListener("scroll", revealOnScroll);
+
+
+
+  const darkToggle = document.getElementById("toogle-dark");
+
+darkToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+    darkToggle.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "light");
+    darkToggle.textContent = "🌙";
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    darkToggle.textContent = "☀️";
+  }
+});
