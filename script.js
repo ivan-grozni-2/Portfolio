2c6b1a645247072f74f9bfeb70d27e3457bf0011
ivan-grozni-2 <3ivangrozni@gmail.com>
2025-08-24 23:03:30 +0300

    update

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 const list = document.getElementById("project-list");
 
+/*async function loadProjects(){
+    try{
+        const GitJSON = await fetch('https://api.github.com/users/ivan-grozni-2/repos')
+        const transJSON = await GitJSON.json();
+
+        const portfolios = transJSON
+            .filter(repo => !["skills-introduction-to-github"].includes(repo.name))
+            .sort((a, b) => new Date(b.update_at) - new Date(a.update_at));
+
+        //portfolios = transJSON;
+
+
+
+        portfolios.forEach(element => {
+            const card = document.createElement('div');
+            card.classList.add('project-card');
+            
+            const description = element.description?.trim() || 'No description provided yet.';
+            const homepage = element.homepage || `https://${element.owner.login}.github.io/${element.name}/`;
+
+            card.innerHTML = `
+                <h3>${formatTitle(element.name)}</h3>
+                <p>${description}</p>
+                <p><strong>Language:</strong> ${element.language || 'N/A'}</p>
+                <a href="${homepage}" target="_blank">Live Demo</a>
+                <a href="${element.html_url}" target="_blank">GitHub</a>
+            `;
+      
+            list.appendChild(card);
+            
+        });
+
+    }
+    catch(error) {
+        list.innerHTML = `<p> error ${error.message}</p>`
+
+    }
+}*/
+
+const GITUSER = "ivan-grozni-2";
+
 async function loadProjects() {
     try{
-        const response = await fetch(`https://api.github.com/users/ivan-grozni-2/repos`);
-        if (!response.ok) {
-            throw new Error(`GitHub API error: ${response.status}`);
-          }
-
+        const response = await fetch(`https:api.github.com/users/${GITUSER}/repos`);
         const repos = await response.json();
 
         const projectRepos = repos.filter(repo => !repo.fork && repo.name!="skills-introduction-to-github");
@@ -19,6 +56,7 @@ async function loadProjects() {
             const card = document.createElement("div");
             card.classList.add("card");
             card.classList.add("reveal");
+            card.setAttribute("href", `${repo.homepage || repo.html_url}` )
 
 
             card.innerHTML = `
@@ -32,8 +70,6 @@ async function loadProjects() {
         });
     } catch(error){
         console.error("Error loading projects:", error);
-        document.getElementById("projects-grid").innerHTML =
-          `<p style="color:red;">⚠️ Could not load projects. (${error.message})</p>`;
     }
     
 }
